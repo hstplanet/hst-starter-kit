6091386c83c43194451df2963220383e72978ed2
umutaltin <umut.altin@live.com>
2021-05-17 23:14:57 +0300

    Umut

--- template/hst.conf.js
+++ template/hst.conf.js
@@ -3,24 +3,24 @@ module.exports = {
     host: "http://localhost/",
 {{#preset.firebase}}
     firebaseConfig: {
-        apiKey: "AIzaSyDKMzGCwXGkibcxz5td59LD17CrJoUVRAc",
-        authDomain: "cetas-164c3.firebaseapp.com",
-        databaseURL: "https://hayalimdekiis-b4bd2-default-rtdb.firebaseio.com",
-        projectId: "cetas-164c3",
-        storageBucket: "cetas-164c3.appspot.com",
-        messagingSenderId: "678488584058",
-        appId: "1:678488584058:web:6ba58fa7e3cf7f995ef0c3",
-        measurementId: "G-2FE53R01TK"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     },
 {{/preset.firebase}}
 {{#preset.hstcore}}
     hstcloud: {
-        apiKey: "dasdad",
-        projectId: "dasdad-3441478484",
-        storageBucket: "6359313338",
-        messagingSenderId: "2820913033",
-        appId: "1:3441478484:web:1620989959022",
-        measurementId: "H-4485436514"
+        apiKey: "{{name}}",
+        projectId: "{{name}}-{{projectId}}",
+        storageBucket: "{{storageBucket}}",
+        messagingSenderId: "{{messagingSenderId}}",
+        appId: "1:{{projectId}}:web:1620989959022",
+        measurementId: "H-{{measurementId}}"
     },
 {{/preset.hstcore}}
     auth: {
