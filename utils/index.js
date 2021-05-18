db8384c8e786e3eea441c3f98a18d08cceca6a23
umutaltin <umut.altin@live.com>
2021-05-19 00:29:04 +0300

    Umut

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const spawn = require('child_process').spawn
-const axios = require('axios');
+const axios = require('../axios');
 
 const lintStyles = ['standard', 'airbnb', 'prettier']
 
