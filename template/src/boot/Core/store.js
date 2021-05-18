bb43163adca6da4c54c38e113e74f5cf4a9d4607
umutaltin <umut.altin@live.com>
2021-05-18 16:21:11 +0300

    Umut

--- template/src/boot/Core/store.js
+++ template/src/boot/Core/store.js
@@ -4,8 +4,7 @@ import boot from "app/hst.conf"
 class Store {
 
     collectionPath = "";
-    path = "http://server.hstplanet.com/api/store/"
-        // http://server.hstplanet.com/api/store/patch?projectId=1399588395&model=deneme
+    path = "http://server.hstplanet.com/api/store"
 
     collection(collection) {
         this.collectionPath = collection;
@@ -14,7 +13,7 @@ class Store {
 
     get() {
         return new Promise((resolve, reject) => {
-            axios.get(this.path + "/get?projectId={{projectId}}&model=" + this.collectionPath).then((res) => {
+            axios.post(this.path + "/get?projectId=1399588395&model=" + this.collectionPath).then((res) => {
                 resolve(res.data);
             }).catch((err) => {
                 reject(err);
@@ -23,13 +22,33 @@ class Store {
     }
 
     where(attr, query, value) {
-        this.collectionPath += "?" + attr + query + value;
+        this.collectionPath += '&where=' + "?" + attr + query + value;
         return this;
     }
 
     set(value) {
         return new Promise((resolve, reject) => {
-            axios.post(boot.host + this.collectionPath, value).then((res) => {
+            axios.post(this.path + "/post?projectId=1399588395&model=" + this.collectionPath, value).then((res) => {
+                resolve(res.data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+
+    update(value) {
+        return new Promise((resolve, reject) => {
+            axios.post(this.path + "/patch?projectId=1399588395&model=" + this.collectionPath, value).then((res) => {
+                resolve(res.data);
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+
+    delete(value) {
+        return new Promise((resolve, reject) => {
+            axios.delete(this.path + "/delete?projectId=1399588395&model=" + this.collectionPath, value).then((res) => {
                 resolve(res.data);
             }).catch((err) => {
                 reject(err);
