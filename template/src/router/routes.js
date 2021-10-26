13393a0df29ebc49a0ddc5c37e668425c8e30256
umutaltin <umut.altin@live.com>
2021-10-26 22:53:35 +0300

    Umut

--- template/src/router/routes.js
+++ template/src/router/routes.js
@@ -16,114 +16,111 @@ const routes = [
             {{ #preset.loginsystem }}
             // Mail Verified
             {
-        beforeEnter: (to, from, next) => {
-            hst.server.auth.onAuthStateChanged().then(user => {
-                if (res.emailStatus !== 'confirmed') {
-                    next();
-                } else {
-                    next("/");
-                }
-            }).catch(err => {
-                next("/");
-            });
-        },
-        path: 'mailverified',
-        component: () =>
-            import('pages/Login/emailVerified.vue'),
-    },
-    // Mail Verified OK
-    {
-        beforeEnter: (to, from, next) => {
-            hst.server.auth.onAuthStateChanged().then(user => {
-                if (res.emailStatus !== 'confirmed') {
-                    next();
-                } else {
-                    next("/");
-                }
-            }).catch(err => {
-                next("/");
-            });
-        },
-        path: 'mailverifiedok',
-        component: () =>
-            import('src/pages/Login/OkeMail.vue')
-    },
-    {{/ preset.loginsystem}}
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        if (res.emailStatus !== 'confirmed') {
+                            next();
+                        } else {
+                            next("/");
+                        }
+                    }).catch(err => {
+                        next("/");
+                    });
+                },
+                path: 'mailverified',
+                component: () =>
+                    import('pages/Login/emailVerified.vue'),
+            },
+            // Mail Verified OK
+            {
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        if (res.emailStatus !== 'confirmed') {
+                            next();
+                        } else {
+                            next("/");
+                        }
+                    }).catch(err => {
+                        next("/");
+                    });
+                },
+                path: 'mailverifiedok',
+                component: () =>
+                    import('src/pages/Login/OkeMail.vue')
+            },
+            {{/ preset.loginsystem }}
         ]
     },
-
-{ {#preset.loginsystem } }
-// Login
-{
-    path: '/login',
+    {{ #preset.loginsystem }}
+    // Login
+    {
+        path: '/login',
         component: () =>
             import('layouts/LoginLayout.vue'),
-            children: [
-                // Giriş
-                {
-                    path: 'signin',
-                    beforeEnter: (to, from, next) => {
-                        hst.server.auth.onAuthStateChanged().then(user => {
-                            next("/");
-                        }).catch(() => {
-                            next();
-                        });
-                    },
-                    component: () =>
-                        import('src/pages/Login/SignIn.vue')
+        children: [
+            // Giriş
+            {
+                path: 'signin',
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        next("/");
+                    }).catch(() => {
+                        next();
+                    });
                 },
-                //Kullanıcı Oluşturma
-                {
-                    path: 'signup',
-                    beforeEnter: (to, from, next) => {
-                        hst.server.auth.onAuthStateChanged().then(user => {
-                            next("/");
-                        }).catch(() => {
-                            next();
-                        });
-                    },
-                    component: () =>
-                        import('pages/Login/SignUp.vue')
-                },
-                // Reset
-                {
-                    path: 'reset',
-                    beforeEnter: (to, from, next) => {
-                        hst.server.auth.onAuthStateChanged().then(user => {
-                            next("/");
-                        }).catch(() => {
-                            next();
-                        });
-                    },
-                    component: () =>
-                        import('pages/Login/reset.vue')
-                },
-                // Password New
-                {
-                    path: 'password-new',
-                    beforeEnter: (to, from, next) => {
-                        hst.server.auth.onAuthStateChanged().then(user => {
-                            next("/");
-                        }).catch(() => {
-                            next();
-                        });
-                    },
-                    component: () =>
-                        import('pages/Login/password-new.vue')
-                },
-            ]
-},
-{
-    {
-        /preset.loginsystem}}
-
-        // Always leave this as last one,
-        // but you can also remove it
-        {
-            path: '*',
                 component: () =>
-                    import('pages/Error404.vue')
-        }
+                    import('src/pages/Login/SignIn.vue')
+            },
+            //Kullanıcı Oluşturma
+            {
+                path: 'signup',
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        next("/");
+                    }).catch(() => {
+                        next();
+                    });
+                },
+                component: () =>
+                    import('pages/Login/SignUp.vue')
+            },
+            // Reset
+            {
+                path: 'reset',
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        next("/");
+                    }).catch(() => {
+                        next();
+                    });
+                },
+                component: () =>
+                    import('pages/Login/reset.vue')
+            },
+            // Password New
+            {
+                path: 'password-new',
+                beforeEnter: (to, from, next) => {
+                    hst.server.auth.onAuthStateChanged().then(user => {
+                        next("/");
+                    }).catch(() => {
+                        next();
+                    });
+                },
+                component: () =>
+                    import('pages/Login/password-new.vue')
+            },
+        ]
+    },
+    {{/ preset.loginsystem }}
+
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '*',
+        component: () =>
+            import('pages/Error404.vue')
+    }
 ]
 
-        export default routes
+export default routes
